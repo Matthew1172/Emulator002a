ec31be7ee299e4d52006000b558deb1bd1ec5f9c
MATTHEW PECKO <mpecko000@citymail.cuny.edu>
2020-11-15 13:55:58 -0500

    Just print each opcode from the .com file

--- Emulator002a/Emulator.c
+++ Emulator002a/Emulator.c
@@ -1,7 +1,6 @@
 #define _CRT_SECURE_NO_DEPRECATE
 #define MAXCODE 0x100000
 #include <stdio.h>
-#include "Help.h"
 
 typedef unsigned char byte;
 typedef unsigned short word;
@@ -21,31 +20,9 @@ main() {
 	fclose(f);
 
 	IP = 0;
-	while (1) {
-		opcode = code[IP++];
-		printf("%02X \t", IP);
-		switch (opcode) {
-		case 0x02:
-			AL = AL + AL;
-			break;
-		case 0xB0:
-			AL = 1;
-			break;
-		case 0xC0:
-			//if (AL < (1 << sizeof(byte) * 8)) leftRotate(AL, 1);
-			//else rightRotate(AL, 1);
-			break;
-		case 0xCD:
-			opcode = code[IP++];
-			if (opcode == 0x20) return 0;
-		//default:
-			//printf("%02X \n", opcode);
-		}
-		printf("AL=%d \n", AL);
-	}
 
 
-/*
+
 	while (1) {
 		opcode = code[IP++];
 		printf("%02X \t", IP);
@@ -58,5 +35,5 @@ main() {
 		}
 		//printf("AL=%d \n", AL);
 	}
-*/
+
 }
