015e35adae38027465b868b38ecf33d9f66d1e9d
MATTHEW PECKO <mpecko000@citymail.cuny.edu>
2020-11-19 02:18:08 -0500

    Progress!

--- Emulator002a/Emulator.cpp
+++ Emulator002a/Emulator.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_DEPRECATE
+#define DEBUG true
 #define MAXCODE 0x100000
-#include <stdio.h>
+#include <iostream>
 
 typedef unsigned char byte;
 typedef unsigned short word;
@@ -11,17 +12,149 @@ word AX = 0x0000, CX = 0x0000, DX = 0x0000, BX = 0x0000, SP = 0x0000, BP = 0x000
 word flags = 0x0000, IP = 0x0000, ES = 0x0000, SS = 0x0000, DS = 0x0000, CS = 0x0000;
 
 byte regs8[8] = { AL, AH, CL, CH, DL, DH, BL, BH };
-byte regs16[8] = { AX, CX, DX, BX, SP, BP, SI, DI };
+word regs16[8] = { AX, CX, DX, BX, SP, BP, SI, DI };
 
 byte code[MAXCODE];
 byte opcode = 0x00;
 
-void cop(byte* opcode);
+void cop(byte opcode);
+void dumpRegisters();
 
-void cop(byte& opcode) {
-	byte o = opcode;
+word combine(byte low, byte hi)
+{
+	word combined = hi << 8 | low;
+	return combined;
+}
 
-	printf("%02X \t", o);
+// Assumes little endian
+void printBits(size_t const size, void const* const ptr)
+{
+	unsigned char* b = (unsigned char*)ptr;
+	unsigned char byte;
+	int i, j;
+
+	for (i = size - 1; i >= 0; i--) {
+		for (j = 7; j >= 0; j--) {
+			byte = (b[i] >> j) & 1;
+			printf("%u", byte);
+		}
+	}
+	puts("");
+}
+
+
+/*
+* 
+* 
+* 00,01,02,03,04,05
+10,11,12,13,14,15
+70,71,72,73,74,75,76,77,78,79,7a,7b,7c,7d,7e,7f
+80,81,83,84,85,86,87
+88,89,8A,8B,8C,8D,8E,8F
+91,92,93,94,95,96,97,98,99,9a,9c,9d,a0,a1,a2,a3,a4,a5,a6,a7
+c6,c7
+e2
+* 
+* 
+* 
+*/
+
+
+void cop(byte opcode) {
+	switch (opcode) {
+	case 0xB0:
+		//mov AL,imm8
+		opcode = code[IP++];
+		AL = opcode;
+		break;
+	case 0xB1:
+		//mov CL,imm8
+		opcode = code[IP++];
+		CL = opcode;
+		break;
+	case 0xB2:
+		//mov DL,imm8
+		opcode = code[IP++];
+		DL = opcode;
+		break;
+	case 0xB3:
+		//mov BL,imm8
+		opcode = code[IP++];
+		BL = opcode;
+		break;
+	case 0xB4:
+		//mov AH,imm8
+		opcode = code[IP++];
+		AH = opcode;
+		break;
+	case 0xB5:
+		//mov CH,imm8
+		opcode = code[IP++];
+		CH = opcode;
+		break;
+	case 0xB6:
+		//mov DH,imm8
+		opcode = code[IP++];
+		DH = opcode;
+		break;
+	case 0xB7:
+		//mov BH,imm8
+		opcode = code[IP++];
+		BH = opcode;
+		break;
+	case 0xB8:
+		//mov AX,imm16
+		opcode = code[IP++];
+		AL = opcode;
+		opcode = code[IP++];
+		AH = opcode;
+		AX = combine(AL, AH);
+		break;
+	case 0xB9:
+		//mov CX,imm16
+		opcode = code[IP++];
+		CL = opcode;
+		opcode = code[IP++];
+		CH = opcode;
+		CX = combine(CL, CH);
+		break;
+	case 0xBA:
+		//mov DX,imm16
+		opcode = code[IP++];
+		DL = opcode;
+		opcode = code[IP++];
+		DH = opcode;
+		DX = combine(DL, DH);
+		break;
+	case 0xBB:
+		//mov BX,imm16
+		opcode = code[IP++];
+		BL = opcode;
+		opcode = code[IP++];
+		BH = opcode;
+		BX = combine(BL, BH);
+		break;
+	case 0xBC:
+		//mov SP,imm16
+		opcode = code[IP++];
+		SP = combine(opcode, code[IP++]);
+		break;
+	case 0xBD:
+		//mov BP,imm16
+		opcode = code[IP++];
+		BP = combine(opcode, code[IP++]);
+		break;
+	case 0xBE:
+		//mov SI,imm16
+		opcode = code[IP++];
+		SI = combine(opcode, code[IP++]);
+		break;
+	case 0xBF:
+		//mov DI,imm16
+		opcode = code[IP++];
+		DI = combine(opcode, code[IP++]);
+		break;
+	}
 }
 
 int main() {
@@ -35,8 +168,6 @@ int main() {
 	flags = 0x00000000;
 	while (1) {
 		opcode = code[IP++];
-
-		//printf("%02X \t", IP);
 		switch (opcode) {
 		case 0x0f:
 			//legacy lock
@@ -163,13 +294,34 @@ int main() {
 			break;
 		case 0xcd:
 			opcode = code[IP++];
+			if (DEBUG) printf("\nRead byte 0xCD followed by terminator byte: %02X\nexiting . . .\n", opcode);
 			if (opcode == 0x20) return 0;
-		default:
-			cop(opcode);
+		default:		
+/*
+			printf("%hhx %zu\n", opcode, sizeof(opcode));
+			byte opcode2 = code[0];
+			word an = opcode + opcode2;
+			int bn = an*0xFFFF;
+			printf("%04X \n", an);
+			printf("%08X \n", bn);
 			//printf("%02X \t", opcode);
+*/
+
+
+			cop(opcode);
 			break;
 		}
-		printf("AL=%d \t AH=%d \t CF=%d \n", AL, AH, flags);
+		if (DEBUG) dumpRegisters();
 	}
 	return 0;
+}
+
+void dumpRegisters() {
+	printf("\n\nIP AFTER INSTRUCTION: %d %10s OPCODE: %02X\n", IP, " ", opcode);
+	printf("AL=%02X \t AH=%02X \t AX=%04X \n", AL, AH, AX);
+	printf("CL=%02X \t CH=%02X \t CX=%04X \n", CL, CH, CX);
+	printf("DL=%02X \t DH=%02X \t DX=%04X \n", DL, DH, DX);
+	printf("BL=%02X \t BH=%02X \t BX=%04X \n", BL, BH, BX);
+	printf("SP=%04X \t BP=%04X \t SI=%04X \t DI=%04X \n", SP, BP, SI, DI);
+	printf("FLAGS: "); printBits(sizeof(flags), &flags);
 }
--- Emulator002a/Help.h
+++ Emulator002a/Help.h
@@ -1,16 +1,131 @@
 #pragma once
-/*Function to left rotate n by d bits*/
-int leftRotate(int n, unsigned int d)
-{
-	/* In n<<d, last d bits are 0. To put first 3 bits of n at
-	  last, do bitwise or of n<<d with n >>(INT_BITS - d) */
-	return (n << d) | (n >> (32 - d));
-}
+#include <iostream>
+#include <string>
+#include <map>
 
-/*Function to right rotate n by d bits*/
-int rightRotate(int n, unsigned int d)
-{
-	/* In n>>d, first d bits are 0. To put last 3 bits of at
-	  first, do bitwise or of n>>d with n <<(INT_BITS - d) */
-	return (n >> d) | (n << (32 - d));
+namespace emulator {
+
+	typedef unsigned char byte;
+	typedef unsigned short word;
+	typedef unsigned int dword;
+
+	class MyByte {
+	private:
+		std::string value;
+		std::map<char, int> hex_value_of_dec(void)
+		{
+			std::map<char, int> m{ { '0', 0 }, { '1', 1 },
+							  { '2', 2 }, { '3', 3 },
+							  { '4', 4 }, { '5', 5 },
+							  { '6', 6 }, { '7', 7 },
+							  { '8', 8 }, { '9', 9 },
+							  { 'A', 10 }, { 'B', 11 },
+							  { 'C', 12 }, { 'D', 13 },
+							  { 'E', 14 }, { 'F', 15 } };
+			return m;
+		}
+		std::map<int, char> dec_value_of_hex(void)
+		{
+			std::map<int, char> m{ { 0, '0' }, { 1, '1' },
+							  { 2, '2' }, { 3, '3' },
+							  { 4, '4' }, { 5, '5' },
+							  { 6, '6' }, { 7, '7' },
+							  { 8, '8' }, { 9, '9' },
+							  { 10, 'A' }, { 11, 'B' },
+							  { 12, 'C' }, { 13, 'D' },
+							  { 14, 'E' }, { 15, 'F' } };
+			return m;
+		}
+	public:
+		MyByte() {
+			setValue("");
+		}
+		MyByte(byte b) {
+			std::string s(1, b);
+			setValue(s);
+		}
+		void setValue(const std::string value) {
+			this->value = value;
+		}
+		std::string getValue() {
+			return this->value;
+		}
+		MyByte operator+(const MyByte& bincoming) {
+			MyByte myb;
+
+			std::string a = getValue();
+			std::string b = bincoming.value;
+
+			std::map<char, int> m = hex_value_of_dec();
+			std::map<int, char> k = dec_value_of_hex();
+
+			// Check if length of string first is 
+			// greater than or equal to string second 
+			if (a.length() < b.length()) swap(a, b);
+
+			
+			std::string ans = "";
+
+			// Initialize carry as zero 
+			int carry = 0, i, j;
+
+			// Traverse till second string 
+			// get traversal completely 
+			for (i = a.length() - 1, j = b.length() - 1;
+				j >= 0; i--, j--) {
+
+				// Decimal value of element at a[i] 
+				// Decimal value of element at b[i] 
+				int sum = m[a[i]] + m[b[j]] + carry;
+
+				// Hexadecimal value of sum%16 
+				// to get addition bit 
+				int addition_bit = k[sum % 16];
+
+				// Add addition_bit to answer 
+				ans.push_back(addition_bit);
+
+				// Update carry 
+				carry = sum / 16;
+			}
+
+			// Traverse remaining element 
+			// of string a 
+			while (i >= 0) {
+
+				// Decimal value of element 
+				// at a[i] 
+				int sum = m[a[i]] + carry;
+
+				// Hexadecimal value of sum%16 
+				// to get addition bit 
+				int addition_bit = k[sum % 16];
+
+				// Add addition_bit to answer 
+				ans.push_back(addition_bit);
+
+				// Update carry 
+				carry = sum / 16;
+				i--;
+			}
+
+			// Check if still carry remains 
+			if (carry) {
+				ans.push_back(k[carry]);
+			}
+
+			// Reverse the final string 
+			// for desired output 
+			reverse(ans.begin(), ans.end());
+
+			// Return the answer
+			//return ans;
+			myb.setValue(ans);
+			return myb;
+		}
+		friend std::ostream& operator<<(std::ostream& os, MyByte const& b) {
+			return os << "I'm in the class, msg=" << b.value << std::endl;
+		}
+
+	};
 }
